cd5098460806b44f13fad0efc4ecdd545b0a9c11
gbever <50702812+gbever@users.noreply.github.com>
2023-07-25 09:45:14 +0200

    5 files modified, 1 added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = '';
+  users: any;
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    /*
+      this.api.get('users/2').subscribe((res) => {
+        this.users = res;
+        console.log('data response', this.users);
+      });
+    */
+    /*
+    this.api.get('users?page=2').subscribe((res) => {
+      this.users = res;
+      console.log('data response', this.users);
+    });
+    */
+  }
 }
