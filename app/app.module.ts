abd484843b8451cc1f41a6fb7d268a98590ae592
gbever <50702812+gbever@users.noreply.github.com>
2023-07-25 12:23:10 +0200

    3 files modified, 1 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, HelloComponent],
+  providers: [HttpClient, ApiService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
